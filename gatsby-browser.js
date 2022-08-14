6d39d41e13f1cb1e5d52d6934bde2330382412c3
magandhi <magandhi@adobe.com>
2022-08-13 17:17:10 -0700

    first commit

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+import "@popperjs/core/dist/umd/popper.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "material-design-kit/dist/material-design-kit.js";
---
+++ src/constants/links.js
@@ -0,0 +1,24 @@
+export default [
+  { path: "/", text: "home" },
+
+  {
+    path: "/about",
+
+    text: "About Us",
+  },
+  {
+    path: "/portfolio",
+
+    text: "Portfolio",
+  },
+  {
+    path: "/blog",
+
+    text: "Blog",
+  },
+  {
+    path: "/contact",
+
+    text: "Contact",
+  },
+]
