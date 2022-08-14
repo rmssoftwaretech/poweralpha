b9325a88f9a8560260833b3400be45035c80bd37
magandhi <magandhi@adobe.com>
2022-08-13 21:45:31 -0700

    updated the templates

---
+++ src/templates/post-template.js
@@ -0,0 +1,87 @@
+import React from "react"
+import * as styles from "../css/postTemplate.module.css"
+import { Link, graphql } from "gatsby"
+import Image from "gatsby-image"
+import Layout from "../components/Layout"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import { FaGithub, FaTwitterSquare, FaDev } from "react-icons/fa"
+
+
+const postTemplate = ({ data }) => {
+    const { title, date, author, twitter, github, dev, image, tags, series, slug } = data.mdx.frontmatter;
+    const { body } = data.mdx;
+    const img = image.childImageSharp.fluid;
+    const url = "https://poweralpha.tech/";
+    const blogIdentity = slug;
+    console.log(blogIdentity);
+    console.log(`${url}${slug}`);
+    let disqusConfig = {
+        url: `${url}${slug}`,
+        identifier: blogIdentity,
+        title: title,
+    }
+    return (
+        <Layout>
+           
+            <section className={styles.template}>
+                <Link to="/" className={styles.link}>
+                    back to all posts
+                </Link>
+                <div className={styles.info}>
+                    <h1>{title}</h1>
+                    <h4>
+                        <span>by {author}</span> / <span>{date}</span>
+                    </h4>
+                    
+                    <h5 className={styles.seriesText}>
+                        {series && <Link to={`/series/${series}`} className={styles.seriesLink}>
+                                    Series: {series}
+                                </Link>
+                        }
+                    </h5>
+                </div>
+                <Image fluid={img} />
+                <div className={styles.content}>
+                    <MDXRenderer>{body}</MDXRenderer>
+                </div>
+                <div className={styles.author}>
+                    <h1>{author}</h1>
+                    {twitter && <a href={twitter} target="_blank" rel="noopener noreferrer">
+                                    {<FaTwitterSquare />}</a>}
+                    {github && <a href={github} target="_blank" rel="noopener noreferrer">{<FaGithub />}</a>}
+                    {dev && <a href={dev} target="_blank" rel="noopener noreferrer">{<FaDev />}</a>}
+                </div>
+               
+              
+            </section>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+query getPost($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+        frontmatter {
+            title
+            slug
+            date(formatString: "MMMM Do, YYYY")
+            author
+            twitter
+            github
+            dev
+            tags
+            series
+            image {
+            childImageSharp {
+                    fluid {
+                    ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+        }
+        body
+    }
+}
+`
+
+export default postTemplate;
